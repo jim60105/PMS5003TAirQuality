e8bdf49198420f93b5957904cef50b80f0282397
Jim <jim60105@gmail.com>
2017-08-14 14:25:07 +0800

    v17.08.14.1 修改: 1.修改DB資料表名稱test->airData

--- php/addData.php
+++ php/addData.php
@@ -10,7 +10,7 @@
     $clientNum = $_GET["clientNum"];
 
     $db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';port='.$port,$dbuser,$dbpass);
-    $stmt = $db->prepare("INSERT INTO test (no, time, pm1, pm10, pm25, temp, humid, clientNum) VALUES (NULL, CURRENT_TIMESTAMP, :pm1, :pm10, :pm25, :temp, :humid, :clientNum);");
+    $stmt = $db->prepare("INSERT INTO airData (no, time, pm1, pm10, pm25, temp, humid, clientNum) VALUES (NULL, CURRENT_TIMESTAMP, :pm1, :pm10, :pm25, :temp, :humid, :clientNum);");
     $stmt->bindValue(':pm1',$pm1);
     $stmt->bindValue(':pm10',$pm10);
     $stmt->bindValue(':pm25',$pm25);
