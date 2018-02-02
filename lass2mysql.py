8ee540745fe4827578b8a0ea20a51a67f1cbd02f
Jim <jim60105@gmail.com>
2018-02-02 16:10:46 +0800

    v18.02.02.0 Add: 1.Store LASS device details to mysql

--- lass2mysql.py
+++ lass2mysql.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-#
+# Author: jim60105@gmail.com
+# Version: v18.02.02.0
 
 import paho.mqtt.client as mqtt
 import re
 import mysql.connector
+import ast
 
 MQTT_SERVER = "gpssensor.ddns.net"
 MQTT_PORT = 1883
@@ -50,10 +52,24 @@ def on_message(client, userdata, msg):
     
     if 'device_id' not in tempDict:
         return
+    else:
+        tempDict['device_id'] = '"' +tempDict['device_id'] + '"'
     
-    tempDict['time'] = '"' + tempDict['date'] + ' ' +tempDict['time'] + '"'
-    tempDict['device_id'] = '"' +tempDict['device_id'] + '"'
+    if 'app' not in tempDict:
+        tempDict['app'] = 'NULL'
+    else:
+        tempDict['app'] = '"' +tempDict['app'] + '"'
+        
+    if 'device' not in tempDict:
+        tempDict['device'] = 'NULL'
+    else:
+        tempDict['device'] = '"' +tempDict['device'] + '"'
     
+    if 'time' not in tempDict:
+        tempDict['time'] = now.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        tempDict['time'] = '"' + tempDict['date'] + ' ' +tempDict['time'] + '"'
+        
     if 's_t0' in tempDict:
         tempDict['temp'] = tempDict['s_t0']
     elif 's_t1' in tempDict:
@@ -94,21 +110,47 @@ def on_message(client, userdata, msg):
         tempDict['s_d0'] = 'NULL'
     if 's_g8' not in tempDict:
         tempDict['s_g8'] = 'NULL'
+        
+    if 'gps_lat' not in tempDict:
+        tempDict['gps_lat'] = 'NULL'
+    if 'gps_lon' not in tempDict:
+        tempDict['gps_lon'] = 'NULL'
+    if 'gps_alt' not in tempDict:
+        tempDict['gps_alt'] = 'NULL'
     
-    # print (str(tempDict), flush=True)
-    
-    query = ('SELECT * FROM `lass` WHERE `device_id` = {device_id} AND `time` = {time}').format(**tempDict)
-    cur.execute(query)
-    rows = cur.fetchall()
-    conn.commit()
-    if len(rows) == 0:
-        query = ('INSERT INTO `lass` '
-            '(`no`, `device_id`, `time`, `pm1`, `pm10`, `pm25`, `temp`, `humid`, `co2`) '
-            'VALUES (NULL, {device_id}, {time}, {s_d2}, {s_d1}, {s_d0}, {temp}, {humid}, {s_g8})').format(**tempDict)
-        # print (query)
+    try:
+        query = ('SELECT * FROM `lassdata` WHERE `device_id` = {device_id} AND `time` = {time}').format(**tempDict)
         cur.execute(query)
+        rows = cur.fetchall()
         conn.commit()
-    tempDict = {}
+        if len(rows) == 0:
+            # 插入資料
+            query = ('INSERT INTO `lassdata` '
+                '(`no`, `device_id`, `time`, `pm1`, `pm10`, `pm25`, `temp`, `humid`, `co2`) '
+                'VALUES (NULL, {device_id}, {time}, {s_d2}, {s_d1}, {s_d0}, {temp}, {humid}, {s_g8})').format(**tempDict)
+            cur.execute(query)
+            conn.commit()
+            
+            # 寫入測站資料    
+            query = ('SELECT `device_id` FROM `lassdevice` WHERE `device_id` = {device_id}').format(**tempDict)
+            cur.execute(query)
+            rows = cur.fetchall()
+            conn.commit()
+            if len(rows) == 0:
+                query = ('INSERT INTO `lassdevice` '
+                '(`device_id`, `app`, `device`, `gps_lat`, `gps_lon`, `gps_alt`) '
+                'VALUES ({device_id}, {app}, {device}, {gps_lat}, {gps_lon}, {gps_alt})').format(**tempDict)
+            else:
+                tempDict['no'] = rows[0]['no']
+                query = ('UPDATE `lassdevice` '
+                'SET `app` = {app}, `device` = {device}, `gps_lat` = {gps_lat}, `gps_lon` = {gps_lon}, `gps_alt` = {gps_alt} '
+                'WHERE `lassdevice`.`device_id` = {device_id}').format(**tempDict)
+            
+            cur.execute(query)
+            conn.commit()
+        tempDict = {}
+    except:
+        return
 
 def on_disconnect(client, userdata,rc=0):
     print("DisConnected result code "+str(rc))
