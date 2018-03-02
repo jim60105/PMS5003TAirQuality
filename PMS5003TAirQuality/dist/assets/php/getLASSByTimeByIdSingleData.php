fb84e295fefbe09befa090717520e0d75b98c8e5
Jim <jim60105@gmail.com>
2018-03-02 16:49:59 +0800

    v18.03.02.2 新增: 1.lass latest data查詢 2.get devices時清理6小時未更新devices

--- PMS5003TAirQuality/dist/assets/php/getLASSByTimeByIdSingleData.php
+++ PMS5003TAirQuality/dist/assets/php/getLASSByTimeByIdSingleData.php
@@ -3,7 +3,9 @@ include('setMySQL.php');
 //include('encrypt.php');
 if(isset($_GET["time"])) {
     $time = $_GET["time"];
+    $getLatest = false;
 }else{
+    $getLatest = true;
     $time = gmdate("Y-m-d H:i:s");
 }
 $device_idList = json_decode($_GET["device_id"],true);
@@ -11,16 +13,20 @@ $device_idList = json_decode($_GET["device_id"],true);
 $arr = array();
 $db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';port='.$port,$dbuser,$dbpass);
 foreach($device_idList as $device_id) {
-    $stmt = $db->prepare("
-        SELECT * FROM lassdata as t
-        INNER JOIN (
-            SELECT lassdata.device_id as device_id2, MAX(lassdata.time) as max_time
-            FROM lassdata
-            WHERE lassdata.time < :timee AND lassdata.device_id = :device_id
-        ) as group_table
-        ON t.device_id = group_table.device_id2 and t.time = group_table.max_time
-    ");
-    $stmt->bindValue(':timee', $time);
+    if($getLatest) {
+        $stmt = $db->prepare("SELECT * FROM `lassdevice` WHERE `device_id` = :device_id");
+    }else {
+        $stmt = $db->prepare("
+            SELECT * FROM lassdata as t
+            INNER JOIN (
+                SELECT lassdata.device_id as device_id2, MAX(lassdata.time) as max_time
+                FROM lassdata
+                WHERE lassdata.time < :timee AND lassdata.device_id = :device_id
+            ) as group_table
+            ON t.device_id = group_table.device_id2 and t.time = group_table.max_time
+        ");
+        $stmt->bindValue(':timee', $time);
+    }
     $stmt->bindValue(':device_id', $device_id);
     $stmt->execute();
     //把data倒進去
--- PMS5003TAirQuality/src/assets/php/getLASSDevices.php
+++ PMS5003TAirQuality/src/assets/php/getLASSDevices.php
@@ -1,9 +1,14 @@
 <?php
 include('setMySQL.php');
 //include('encrypt.php');
+
+//清理過期devices
+$db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';port='.$port,$dbuser,$dbpass);
+$stmt = $db->prepare("DELETE FROM `lassdevice` WHERE `time` < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 6 HOUR)");
+$stmt->execute();
+
 $arr = array();
 
-$db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';port='.$port,$dbuser,$dbpass);
 $stmt = $db->prepare("SELECT * FROM `lassdevice` ORDER BY `gps_lat` ASC");
 $stmt->execute();
 
