ddb1591a2ef528bf52ae7a244f9dfa3b7707fe00
Jim <jim60105@gmail.com>
2018-05-10 10:17:35 +0800

    v18.05.10.0 1.addData.php增加處理airdatadevice

--- PMS5003TAirQuality/dist/assets/php/addData.php
+++ PMS5003TAirQuality/dist/assets/php/addData.php
@@ -20,6 +20,15 @@ if(isset($_GET["clientNum"])){
     $stmt->bindValue(':humid',$humid);
     $stmt->bindValue(':clientNum',$clientNum);
     $stmt->execute();
+
+    $stmt = $db->prepare("UPDATE airdatadevice SET time=CURRENT_TIMESTAMP, pm1=:pm1, pm10=:pm10, pm25=:pm25, temp=:temp, humid=:humid WHERE clientNum=:clientNum;");
+    $stmt->bindValue(':pm1',$pm1);
+    $stmt->bindValue(':pm10',$pm10);
+    $stmt->bindValue(':pm25',$pm25);
+    $stmt->bindValue(':temp',$temp);
+    $stmt->bindValue(':humid',$humid);
+    $stmt->bindValue(':clientNum',$clientNum);
+    $stmt->execute();
 }
 
 
