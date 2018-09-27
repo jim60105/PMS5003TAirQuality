32afa3ce3aae7cdd4e0984b6e276ec71be170196
Jim <jim60105@gmail.com>
2018-09-27 15:32:19 +0800

    v18.09.27.0 新增: 1.delete1MonthBefore定時清除資料

---
+++ delete1MonthBefore.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Author: jim60105@gmail.com
+# Version: v18.09.27.0
+
+import paho.mqtt.client as mqtt
+import mysql.connector
+            
+f = open("dbpassword.pas","r") 
+dbPas = str(f.readline())[0:-1]
+f.close()
+
+conn = mysql.connector.connect(
+         user='air',
+         password=dbPas,
+         host='127.0.0.1',
+         database='air')
+ 
+cur = conn.cursor(dictionary=True)
+
+query = ('DELETE FROM lassdata WHERE `time` < (NOW() - INTERVAL 1 MONTH)')
+cur.execute(query)
+conn.commit()
+
+query = ('DELETE FROM lassdataqueue WHERE `time` < (NOW() - INTERVAL 1 MONTH)')
+cur.execute(query)
+conn.commit()
+
+query = ('DELETE FROM lassdata WHERE `time` > (NOW() + INTERVAL 1 DAY)')
+cur.execute(query)
+conn.commit()
+
+query = ('DELETE FROM lassdataqueue WHERE `time` > (NOW() + INTERVAL 1 DAY)')
+cur.execute(query)
+conn.commit()
