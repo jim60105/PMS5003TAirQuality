c88a45bf50d29adde6fed982855caf1ca615a2c5
Jim <jim60105@gmail.com>
2019-01-16 11:26:07 +0800

    v19.01.16.0 Edit: copy dist to htdocs

--- copyDistToHtdocs.sh
+++ copyDistToHtdocs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cp -r ./PMS5003TAirQuality/dist/.htaccess /htdocs/
-cp -r ./PMS5003TAirQuality/dist/* /htdocs/
+cp -r ./PMS5003TAirQuality/dist/.htaccess /htdocs/PMS5003TAirQuality/
+cp -r ./PMS5003TAirQuality/dist/* /htdocs/PMS5003TAirQuality/
+cp -r ./lass2mysql.sh /htdocs/PMS5003TAirQuality/
 exit 0
