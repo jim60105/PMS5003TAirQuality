0051442f75df50f270392d539063f0c4d14ee58b
Jim <jim60105@gmail.com>
2019-01-16 11:33:50 +0800

    v19.01.16.1 Edit: copy to htdocs add lass2mysql.py

--- copyDistToHtdocs.sh
+++ copyDistToHtdocs.sh
@@ -2,4 +2,5 @@
 cp -r ./PMS5003TAirQuality/dist/.htaccess /htdocs/PMS5003TAirQuality/
 cp -r ./PMS5003TAirQuality/dist/* /htdocs/PMS5003TAirQuality/
 cp -r ./lass2mysql.sh /htdocs/PMS5003TAirQuality/
+cp -r ./lass2mysql.py /htdocs/PMS5003TAirQuality/
 exit 0
